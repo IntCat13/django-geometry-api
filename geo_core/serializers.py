ddfcc880212e2ce5801eaaa09c2dc70218b8f9df
IntCat13 <cat.strecalt@gmail.com>
2024-06-16 20:24:56 +0200

    Add endpoints lines, polygons
    Add endpoints for managing other geometries
    
    Implement new serializers and views

--- geo_core/serializers.py
+++ geo_core/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
-from django.contrib.gis.geos import Point as DPoint
-from .models import Point
+from django.contrib.gis.geos import Point as DPoint, LineString as DLineString, Polygon as DPolygon, LinearRing
+from .models import Point, LineString, Polygon
 
 class PointSerializer(serializers.ModelSerializer):
     coordinates = serializers.ListField(write_only=True)
@@ -18,5 +18,44 @@ class PointSerializer(serializers.ModelSerializer):
         return {
             'id': instance.id,
             'type': 'Point',
+            'coordinates': instance.geom.coords
+        }
+
+class LineStringSerializer(serializers.ModelSerializer):
+    coordinates = serializers.ListField(write_only=True)
+
+    class Meta:
+        model = LineString
+        fields = ('id', 'coordinates')
+
+    def create(self, validated_data):
+        coordinates = validated_data.pop('coordinates')
+        linestring = DLineString(coordinates, srid=4326)
+        return LineString.objects.create(geom=linestring)
+
+    def to_representation(self, instance):
+        return {
+            'id': instance.id,
+            'type': 'LineString',
+            'coordinates': instance.geom.coords
+        }
+
+class PolygonSerializer(serializers.ModelSerializer):
+    coordinates = serializers.ListField(write_only=True)
+
+    class Meta:
+        model = Polygon
+        fields = ('id', 'coordinates')
+
+    def create(self, validated_data):
+        coordinates = validated_data.pop('coordinates')
+        linear_ring = LinearRing(coordinates[0])
+        polygon = DPolygon(linear_ring, srid=4326)
+        return Polygon.objects.create(geom=polygon)
+
+    def to_representation(self, instance):
+        return {
+            'id': instance.id,
+            'type': 'Polygon',
             'coordinates': instance.geom.coords
         }
--- geo_core/views.py
+++ geo_core/views.py
@@ -2,9 +2,17 @@ from rest_framework import viewsets
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
-from .models import Point
-from .serializers import PointSerializer
+from .models import Point, LineString, Polygon
+from .serializers import PointSerializer, LineStringSerializer, PolygonSerializer
 
 class PointViewSet(viewsets.ModelViewSet):
     queryset = Point.objects.all()
     serializer_class = PointSerializer
+
+class LineStringViewSet(viewsets.ModelViewSet):
+    queryset = LineString.objects.all()
+    serializer_class = LineStringSerializer
+
+class PolygonViewSet(viewsets.ModelViewSet):
+    queryset = Polygon.objects.all()
+    serializer_class = PolygonSerializer
