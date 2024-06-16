de41be396961fccd133b904bea9e563ec09c5ed1
IntCat13 <cat.strecalt@gmail.com>
2024-06-16 22:37:45 +0200

    Add join_lines feature
    Add view and service for lines joining

--- geo_core/urls.py
+++ geo_core/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import PointViewSet, LineStringViewSet, PolygonViewSet
+from .views import PointViewSet, LineStringViewSet, PolygonViewSet, join_lines_view
 
 router = DefaultRouter()
 router.register(r'points', PointViewSet)
@@ -9,4 +9,5 @@ router.register(r'polygons', PolygonViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('join_lines/', join_lines_view, name='join_lines'),
 ]
